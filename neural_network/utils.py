768f5b4143b94efe459366d4d40d9dd4bf95913d
Arshad Javeed <37022146+arshad171@users.noreply.github.com>
2023-11-09 23:18:02 +0100

    utils

--- neural_network/utils.py
+++ neural_network/utils.py
@@ -29,7 +29,7 @@ def predict(neural_net: List[BaseLayer], X_data: np.ndarray, proba=True) -> np.n
 
 
 def accuracy(neural_net: List[BaseLayer], X_data: np.ndarray, y_data: np.ndarray) -> np.float32:
-    y_preds = predict(neural_net=neural_net, X_data=X_data)
+    y_preds = predict(neural_net=neural_net, X_data=X_data, proba=False)
     y_preds = np.argmax(y_preds, axis=1)
     y_true = np.argmax(y_data, axis=1)
     
