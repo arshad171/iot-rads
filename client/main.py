0946cd396328974bd6c54a39d7c137d7e3d7e023
alecello <alessandro.celoria@hotmail.com>
2023-11-23 15:43:41 +0100

    Use "with" to manage serial port

--- client/main.py
+++ client/main.py
@@ -1,15 +1,14 @@
 from protocol import SerialPort,Packet,DataType
 from exceptions import *
 
-port = SerialPort("/dev/ttyACM0",9600,1)
+with SerialPort("/dev/ttyACM0",9600,1) as port:
+    while True:
+        try:
+            packet = port.read_packet()
 
-while True:
-    try:
-        packet = port.read_packet()
+            # Print logs
+            if packet is not None and packet.dtype == DataType.LOG:
+                print(packet.data.decode("ascii"))
 
-        # Print logs
-        if packet is not None and packet.dtype == DataType.LOG:
-            print(packet.data.decode("ascii"))
-
-    except MalformedPacketException as e:
-        pass # Just ignore them (for now)
+        except MalformedPacketException as e:
+            pass # Just ignore them (for now)
