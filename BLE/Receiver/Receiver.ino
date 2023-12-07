4adccab81b09e48b30ff85b43e3436ca4fbde1a8
valentin2431 <valentinlopezjimenez@gmail.com>
2023-12-07 21:01:06 +0100

    Float receiver

--- BLE/Receiver/Receiver.ino
+++ BLE/Receiver/Receiver.ino
@@ -1,14 +1,24 @@
 #include <ArduinoBLE.h>
 byte value = 0;
+byte Byte_Array[100] = {0};
+float Float_Array[100] = {0.0};
+int buffer_size = 0;
 
-void byteArrayToFloatArray(byte *byteArray, float *floatArray, int size) {
+/*void byteArrayToFloatArray(byte *byteArray, float *floatArray, int size) {
   for (int i = 0; i < size; i++) {
     memcpy(&floatArray[i], &byteArray[i * sizeof(float)], sizeof(float));
   }
+}*/
+
+void byteArrayToFloatArray(byte* byteArray, float* floatArray, int arrayLength) {
+  // Use memcpy to copy the byte array to the float array
+  memcpy(floatArray, byteArray, arrayLength * sizeof(float));
 }
 
 
-void printFloatArray(float *floatArray, int size) {
+
+
+/*void printFloatArray(float *floatArray, int size) {
   for (int i = 0; i < size; i++) {
     Serial.print(floatArray[i], 2); // Print with 2 decimal places
     Serial.print(" ");
@@ -22,7 +32,7 @@ void printByteArray(byte *byteArray, int size) {
     Serial.print(" ");
   }
   Serial.println();
-}
+}*/
 
 void exploreCharacteristic(BLECharacteristic characteristic) {
   // print the UUID and properties of the characteristic
@@ -38,8 +48,16 @@ void exploreCharacteristic(BLECharacteristic characteristic) {
 
     if (characteristic.valueLength() > 0) {
       // print out the value of the characteristic
-      Serial.print(", value 0x");
-      printData(characteristic.value(), characteristic.valueLength());
+      Serial.println(", value 0x");
+      //printData(characteristic.value(), characteristic.valueLength());
+      buffer_size = characteristic.valueLength();
+      characteristic.readValue(Byte_Array,100);
+      byteArrayToFloatArray(Byte_Array,Float_Array,100 );
+
+      Serial.println("My float values:  ");
+      Serial.println(Float_Array[0]);
+      Serial.println(Float_Array[1]);
+      Serial.println(Float_Array[2]);
     }
   }
   Serial.println();
@@ -78,6 +96,7 @@ void printData(const unsigned char data[], int length) {
     Serial.print(b, HEX);
   }
 }
+
 
 void setup()
 {
