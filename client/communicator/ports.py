6e5bd779900bf60de40b5f66721676bc593aa752
alecello <alessandro.celoria@hotmail.com>
2023-12-07 18:47:26 +0100

    Created a protocol wrapper to neatly handle different command and data types

--- client/communicator/ports.py
+++ client/communicator/ports.py
@@ -1,50 +1,107 @@
 """ Communication ports for the RADS network """
 
+from threading import Thread,Event
 from queue import SimpleQueue
-from threading import Thread
 import struct
+import time
 
 from serial import Serial,SerialException,EIGHTBITS,STOPBITS_ONE,PARITY_NONE
-from exceptions import SerialPortException,MalformedPacketException
 from communicator.protocol import Packet,DataType,Command
+
+from exceptions import (
+    MalformedPacketException,
+    InvalidCommandException,
+    InvalidDataTypeException,
+    SerialPortException
+)
 
 
 class SerialPort(Thread):
-    """" Implements a serial port (together with the RADS protocol) """
-    def __init__(self, port: str = None, baud: int = 9600):
-        # Initialize the two letterboxes
+    """ Implements a serial port (together with the RADS protocol) """
+    def __init__(self, port: str, baud: int, retry: float):
+        # Store serial port parameters
+        self.__port = port
+        self.__baud = baud
+        self.__retry = retry
+
+        # The two queues act like letterboxes
         self.inbox = SimpleQueue()
         self.outbox = SimpleQueue()
 
-        # Setup the serial connection
-        try:
-            self.serial = Serial(port,baud,timeout=0.1,parity=PARITY_NONE,
-                                 bytesize=EIGHTBITS,stopbits=STOPBITS_ONE)
-            self.open = True
-        except SerialException as e:
-            raise SerialPortException("Unable to open serial port") from e
+        # Initialize a dummy serial object
+        self.__serial = Serial()
 
-        # Initiate thread
+        # Thread stop signaling logic
+        self.__stop_event = Event()
+        self.__is_stopped = False
+
+        # Start the thread
         super().__init__(name="Serial Port Connection")
         self.start()
 
-    def run(self):
-        if not self.open:
-            raise SerialPortException("Serial port state invalid.")
+    def __open_serial(self):
+        print("******** Opening Serial Port")
+        while not self.__serial.is_open():
+            try:
+                self.__serial = Serial(self.__port,self.__baud,timeout=0.1,parity=PARITY_NONE,
+                                       bytesize=EIGHTBITS,stopbits=STOPBITS_ONE)
+                continue
+            except SerialException:
+                time.sleep(self.__retry)
 
-        while True:
+    def __close_serial(self):
+        try:
+            self.__serial.close()
+        except SerialException:
+            print("******** Failed to close serial port.")
+
+    def run(self):
+        # Clear the stop event in case we are resuming
+        self.__stop_event.clear()
+
+        # Open the serial port
+        self.__open_serial()
+
+        while not self.__stop_event.is_set():
             # Check whether we have packets to send
             while not self.outbox.empty():
                 self.send_packet(self.outbox.get(False))
 
-            # Process any incoming packet
-            if (p := self.read_packet()) is not None:
-                self.inbox.put(p)
+            try:
+                if (p := self.read_packet()) is not None:
+                    self.inbox.put(p)
+            except InvalidDataTypeException as e:
+                print(f"******** Received data of invalid type 0x{e.id:08x}")
+            except InvalidCommandException as e:
+                print(f"******** Received invalid command 0x{e.id:08x}")
+            except MalformedPacketException as e:
+                print("******** Received malformed packet")
+            except SerialPortException:
+                print("******** Serial communication crashed: restarting...")
+                self.__close_serial()
+                self.__open_serial()
+            except Exception as e:
+                print("******** Unknown exception while handling serial communication!")
+                print(e)
+
+                self.stop()
+
+        # Close the serial port and signal we are ready
+        self.__close_serial()
+        self.__is_stopped = True
+
+    def stop(self):
+        """ Stop the thread """
+        self.__stop_event.set()
+
+    def stopped(self) -> bool:
+        """ Wether the thread has stopped """
+        return self.__is_stopped
 
     def send_packet(self, packet: Packet):
         """ Send a packet over the serial port """
         try:
-            self.serial.write(packet.serialize())
+            self.__serial.write(packet.serialize())
         except SerialException as e:
             raise SerialPortException("Unable to send data through the serial port") from e
 
@@ -52,7 +109,7 @@ class SerialPort(Thread):
         """ Receive a packet from the serial port (if any) """
         try:
             # Read the stream until we find a MAGIC
-            lead = self.serial.read_until(expected=Packet.magic)
+            lead = self.__serial.read_until(expected=Packet.magic)
 
             # Validate that we actually got a good MAGIC
             if len(lead) == 0:
@@ -61,7 +118,7 @@ class SerialPort(Thread):
                 raise MalformedPacketException("Invalid MAGIC received")
 
             header_format = "<III"  # (uint,size_t,uint)
-            header_data = self.serial.read(struct.calcsize(header_format))
+            header_data = self.__serial.read(struct.calcsize(header_format))
 
             if len(header_data) != struct.calcsize(header_format):
                 raise MalformedPacketException("Invalid header received")
@@ -70,7 +127,7 @@ class SerialPort(Thread):
             cmd_id, data_size, type_id = struct.unpack(header_format, header_data)
 
             # Now we can read the data
-            payload = self.serial.read(data_size)
+            payload = self.__serial.read(data_size)
             if len(payload) != data_size:
                 raise MalformedPacketException("Incomplete data stream")
 
--- client/communicator/protocol.py
+++ client/communicator/protocol.py
@@ -3,14 +3,13 @@
 from enum import Enum
 import struct
 
-# Communication and encoding/decoding exceptions
+from communicator.ports import SerialPort
 from exceptions import (
     InvalidCommandException,
     InvalidDataTypeException,
 )
 
 
-# Different kinds of data that can be exchanged
 class DataType(Enum):
     """ Type of data being transmitted """
 
@@ -42,13 +41,13 @@ class Command(Enum):
     SET_TRACK_MASK = (2, "Set railroad mask")
     REPORT_ANOMALY = (3, "Report Anomaly")
 
-    def __init__(self, cid, desc):
+    def __init__(self, cid, label):
         self.id = cid
-        self.desc = desc
+        self.label = label
 
     @classmethod
     def by_id(cls, cid: int):
-        """ Get a Command object by ID """
+        """Get a Command object by ID"""
         candidates = [m for m in cls if m.value[0] == cid]
 
         if len(candidates) == 0:
@@ -56,7 +55,6 @@ class Command(Enum):
         return candidates[0]
 
 
-# Protocol packet
 class Packet:
     """ Network communication packet """
 
@@ -70,8 +68,66 @@ class Packet:
         self.dtype = dtype
 
     def serialize(self) -> bytes:
-        """ Transform the packet into a byte array ready to be transmitted """
+        """Transform the packet into a byte array ready to be transmitted"""
         return struct.pack(Packet.format.format(len(self.data)), self.data)
 
     def __repr__(self) -> str:
-        return f"PACKET: Type <{self.dtype.label}> Command <{self.command.desc}> (Payload size 0x{len(self.data):0x})"
+        return f"PACKET: Type <{self.dtype.label}> Command <{self.command.label}> (Payload size 0x{len(self.data):0x})"
+
+
+class Protocol:
+    """ Implements the RADS protocol """
+
+    def __init__(self, port: str, baud: int = 9600, retry: float = 1):
+        self.serial = SerialPort(port,baud,retry)
+
+        # Handler registries
+        self.cmd_handlers = {}
+        self.type_handlers = {}
+
+    def register_cmd(self, command: Command):
+        """ Register a command handler for a given command """
+        cmd_id = command.value[0]
+
+        # Perform the handler registration
+        def decorator(f):
+            if cmd_id not in self.cmd_handlers:
+                self.cmd_handlers[cmd_id] = [f]
+            else:
+                self.cmd_handlers[cmd_id].append(f)
+
+        return decorator
+
+    def register_type(self, dtype: DataType):
+        """ Register a type handler for a given type """
+        type_id = dtype.value[0]
+
+        if type_id in self.type_handlers:
+            raise ValueError("Cannot register two handlers for the same type")
+        else:
+            self.type_handlers[type_id] = f
+
+    def handle(self) -> Packet | None:
+        """ Handle any incoming packet using command handlers """
+        while not self.serial.inbox.empty():
+            packet: Packet = self.serial.inbox.get()
+            dtype = packet.dtype
+            cmd = packet.command
+
+            # If we have a data type handler use it
+            data = (
+                self.type_handlers[dtype.id](packet.data)
+                if dtype.id in self.type_handlers
+                else data
+            )
+
+            # Call every registered function handler
+            if cmd.id in self.cmd_handlers:
+                for handler in self.cmd_handlers:
+                    handler(data)
+            else:
+                print(f"******** No command Handler defined for Command <{cmd.label}>")
+
+    def send(self, packet: Packet):
+        """ Send a packet to the device """
+        self.serial.inbox.put(packet)
