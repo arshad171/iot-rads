c43148c6b0451def716522d9774c94ad2c6405bd
Arshad Javeed <37022146+arshad171@users.noreply.github.com>
2023-12-07 21:00:10 +0100

    sending a float array works!

--- BLE/Sender/Sender.ino
+++ BLE/Sender/Sender.ino
@@ -5,15 +5,31 @@ const int BUFFER_SIZE = 5;
 const int SIZE_IN_BYTES = BUFFER_SIZE * sizeof(byte);
 byte buffer[BUFFER_SIZE] = {1, 2, 3, 4, 5};
 
+float floatArray[] = {1.0, 2.0, 3.0, 4.0, 5.0};
+const int arrayLength = sizeof(floatArray) / sizeof(float);
+const int byteArraySize = arrayLength * sizeof(float);
+byte byteArray[byteArraySize] = {0};
+
 BLEService service1("19B10000-E8F2-537E-4F6C-D104768A1214"); // Bluetooth® Low Energy LED Service
 
 // BLEByteCharacteristic characteristic1("weights", BLERead | BLEWrite | BLEBroadcast | BLENotify);
 // BLEByteCharacteristic characteristic1("2A37", BLERead | BLEWrite | BLEBroadcast | BLENotify);
-BLECharacteristic characteristic1("2A37", BLERead | BLEWrite | BLEBroadcast | BLENotify, SIZE_IN_BYTES);
+BLECharacteristic characteristic1("2A37", BLERead | BLEWrite | BLEBroadcast | BLENotify, byteArraySize);
 
+
+void floatArrayToByteArray(float* floatArray, byte* byteArray, int arrayLength) {
+  // Use memcpy to copy the float array to the byte array
+  memcpy(byteArray, floatArray, arrayLength * sizeof(float));
+}
+
+void byteArrayToFloatArray(byte* byteArray, float* floatArray, int arrayLength) {
+  // Use memcpy to copy the byte array to the float array
+  memcpy(floatArray, byteArray, arrayLength * sizeof(float));
+}
 
 void setup()
 {
+    floatArrayToByteArray(floatArray, byteArray, arrayLength);
     Serial.begin(9600);
     while (!Serial)
         ;
@@ -39,7 +55,8 @@ void setup()
 
     // set the initial value for the characeristic:
     // characteristic1.writeValue(buffer);
-    characteristic1.writeValue(buffer, SIZE_IN_BYTES);
+    // characteristic1.writeValue(buffer, SIZE_IN_BYTES);
+    characteristic1.writeValue(byteArray, byteArraySize);
 
     // start advertising
     BLE.advertise();
@@ -55,7 +72,8 @@ void loop()
       while(central.connect()) {
         Serial.println("sending data");
         // characteristic1.writeValue(buffer);
-        characteristic1.writeValue(buffer, SIZE_IN_BYTES);
+        // characteristic1.writeValue(buffer, SIZE_IN_BYTES);
+        characteristic1.writeValue(byteArray, byteArraySize);
         delay(1000);
       }
     }
