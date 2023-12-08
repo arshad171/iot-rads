eb6a4286da52379aef0aff0c5eeae01e87965239
alecello <alessandro.celoria@hotmail.com>
2023-12-08 17:27:02 +0100

    Add data capsules convenience function

--- platform/src/utils/types.h
+++ platform/src/utils/types.h
@@ -16,7 +16,7 @@ struct RichMatrix {
 };
 
 template<int r, int c, typename t>
-RichMatrix *BLAtoMatrix(BLA::Matrix<r,c,t> *matrix) {
+RichMatrix *BLAtoRichMatrix(BLA::Matrix<r,c,t> *matrix) {
     size_t data_size = r * c * sizeof(t);
     RichMatrix *capsule = (RichMatrix *) memalloc(sizeof(MatrixMetadata) + data_size);
 
@@ -28,6 +28,14 @@ RichMatrix *BLAtoMatrix(BLA::Matrix<r,c,t> *matrix) {
     // Copy the matrix data
     memcpy(capsule->data,matrix->storage,data_size);
     return capsule;
+}
+
+size_t getRichMatrixSize(RichMatrix *matrix) {
+    uint16_t r = matrix->metadata.rows;
+    uint16_t c = matrix->metadata.cols;
+    size_t s = matrix->metadata.element_size;
+
+    return sizeof(MatrixMetadata) + r*c*s;
 }
 
 struct ImageMetadata {
@@ -52,4 +60,13 @@ RichImage *initImage(uint16_t w, uint16_t h, byte c, size_t d) {
     image->metadata.depth = d;
 
     return image;
+}
+
+size_t getRichImageSize(RichImage *image) {
+    uint16_t w = image->metadata.width;
+    uint16_t h = image->metadata.height;
+    byte c = image->metadata.channels;
+    size_t d = image->metadata.depth;
+
+    return w*h*c*d;
 }
