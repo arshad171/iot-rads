bb1d974b62870b738bb423994d0484c64277107c
alecello <alessandro.celoria@hotmail.com>
2023-12-08 16:44:24 +0100

    Apparently templates cannot be declared...

--- platform/src/utils/types.h
+++ platform/src/utils/types.h
@@ -1,5 +1,7 @@
 #pragma once
+#include "../utils/common.h"
 #include <Arduino.h>
+#include <stdlib.h>
 #include <BasicLinearAlgebra.h>
 
 struct MatrixMetadata {
@@ -8,13 +10,25 @@ struct MatrixMetadata {
     size_t element_size;
 };
 
-struct Matrix {
+struct RichMatrix {
     MatrixMetadata metadata;
     byte data[];
 };
 
 template<int r, int c, typename t>
-Matrix *BLAtoMatrix(BLA::Matrix<r,c,t> matrix);
+RichMatrix *BLAtoMatrix(BLA::Matrix<r,c,t> *matrix) {
+    size_t data_size = r * c * sizeof(t);
+    RichMatrix *capsule = (RichMatrix *) memalloc(sizeof(MatrixMetadata) + data_size);
+
+    // Fill in the metadata
+    capsule->metadata.cols = c;
+    capsule->metadata.rows = c;
+    capsule->metadata.element_size = sizeof(t);
+
+    // Copy the matrix data
+    memcpy(capsule->data,matrix->storage,data_size);
+    return capsule;
+}
 
 struct ImageMetadata {
     uint16_t width;
@@ -23,9 +37,19 @@ struct ImageMetadata {
     size_t depth;
 };
 
-struct Image {
+struct RichImage {
     ImageMetadata metadata;
     byte data[];
 };
 
-Image *initImage(uint16_t w, uint16_t h, byte c, size_t d);
+RichImage *initImage(uint16_t w, uint16_t h, byte c, size_t d) {
+    RichImage *image = (RichImage *) memalloc(sizeof(ImageMetadata) + w*h*c*d);
+
+    // Fill in the metadata
+    image->metadata.channels = c;
+    image->metadata.width = w;
+    image->metadata.height = h;
+    image->metadata.depth = d;
+
+    return image;
+}
