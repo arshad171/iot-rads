1b3b045bd575a8ce7fb5c4ca5b4a1462d0f90225
alecello <alessandro.celoria@hotmail.com>
2023-12-11 12:23:16 +0100

    Implement shutter in client

--- client/ui/windows/main.py
+++ client/ui/windows/main.py
@@ -13,7 +13,7 @@ from PyQt6.QtCore import QThread,pyqtSignal,pyqtSlot
 
 from ui.glob import GLOBAL_SIGNALS
 
-from communicator.format import Command
+from communicator.format import Packet,Command,DataType
 from communicator.protocol import Protocol
 from communicator.ports import SerialChannel
 
@@ -32,17 +32,21 @@ class MainWindow(QMainWindow):
     signal_flush_log     = pyqtSignal()
     signal_status_update = pyqtSignal(str)
 
-    def __init__(self):
+    def __init__(self,handler: Protocol):
         super().__init__() # Initialize the Qt interface
         uic.loadUi(os.path.join("ui","designer","main.ui"),self)
         self.show()
         self.setWindowTitle("RADS Client")
+
+        # Protocol handler (for sending commands)
+        self.__handler = handler
 
         # Load handles to relevant window components
         self.__log_txt = self.findChild(QPlainTextEdit,"log_txt")
         self.__status = self.findChild(QStatusBar,"statusbar")
         self.__frame = self.findChild(QLabel,"frame_lbl")
         self.__save_btn = self.findChild(QPushButton,"save_btn")
+        self.__shutter = self.findChild(QPushButton,"shutter_btn")
 
         # Pictures received from Arduino
         self.__picture = None
@@ -57,6 +61,7 @@ class MainWindow(QMainWindow):
 
         # Register UI signal handlers
         self.__save_btn.clicked.connect(self.save_picture)
+        self.__shutter.clicked.connect(self.get_frame)
 
         # Create the signal dictionary
         self.command_signals = {
@@ -94,6 +99,10 @@ class MainWindow(QMainWindow):
         if fname:
             self.__picture.save(fname,"PNG")
 
+    @pyqtSlot()
+    def get_frame(self):
+        self.__handler.send(Packet(None,Command.GET_FRAME,DataType.LOG))
+
 
 class MainThread(QThread):
     """ Main thread of the client """
@@ -103,9 +112,6 @@ class MainThread(QThread):
         # Initialize PyQt interface
         super().__init__()
 
-        # Spawn the window
-        self.__window = MainWindow()
-
         # Setup the stop event
         self.__stop_event = Event()
 
@@ -113,6 +119,9 @@ class MainThread(QThread):
         self.__handler = Protocol(SerialChannel("/dev/ttyACM0",19200,self.INTERVAL))
         cmd_handlers.register_command_handlers(self.__handler)
         type_handlers.register_datatype_handlers(self.__handler)
+
+        # Spawn the window
+        self.__window = MainWindow(self.__handler)
 
         # Register the QT signals with the protocol handler
         for cmd,sig in self.__window.command_signals.items():
