f72882ef95488e8811490d84865238f25a641d51
alecello <alessandro.celoria@hotmail.com>
2023-12-11 10:42:23 +0100

    Add save functionality

--- client/ui/windows/main.py
+++ client/ui/windows/main.py
@@ -1,10 +1,14 @@
 import os
 import time
+import typing
+
+from threading import Event
 
 from PIL import Image as PILImage
 from PIL.Image import Image
 
-from PyQt6 import uic, QtWidgets
+from PyQt6 import QtGui, uic
+from PyQt6.QtWidgets import QMainWindow,QPlainTextEdit,QStatusBar,QLabel,QFileDialog,QPushButton
 from PyQt6.QtCore import QThread,pyqtSignal,pyqtSlot
 
 from ui.glob import GLOBAL_SIGNALS
@@ -17,8 +21,9 @@ import handlers.command_handlers as cmd_handlers
 import handlers.datatype_handlers as type_handlers
 
 
-class MainWindow(QtWidgets.QMainWindow):
+class MainWindow(QMainWindow):
     """ Main window of the client """
+
     # Define command-related signals
     __signal_write_log = pyqtSignal(str)
     __signal_set_frame = pyqtSignal(Image)
@@ -31,11 +36,16 @@ class MainWindow(QtWidgets.QMainWindow):
         super().__init__() # Initialize the Qt interface
         uic.loadUi(os.path.join("ui","designer","main.ui"),self)
         self.show()
+        self.setWindowTitle("RADS Client")
 
         # Load handles to relevant window components
-        self.__log_txt = self.findChild(QtWidgets.QPlainTextEdit,"log_txt")
-        self.__status = self.findChild(QtWidgets.QStatusBar,"statusbar")
-        self.__frame = self.findChild(QtWidgets.QLabel,"frame_lbl")
+        self.__log_txt = self.findChild(QPlainTextEdit,"log_txt")
+        self.__status = self.findChild(QStatusBar,"statusbar")
+        self.__frame = self.findChild(QLabel,"frame_lbl")
+        self.__save_btn = self.findChild(QPushButton,"save_btn")
+
+        # Pictures received from Arduino
+        self.__picture = None
 
         # Register command signal handlers
         self.__signal_write_log.connect(self.append_log)
@@ -44,6 +54,9 @@ class MainWindow(QtWidgets.QMainWindow):
         # Register global signal handlers
         GLOBAL_SIGNALS.status_signal.connect(self.status_update)
         GLOBAL_SIGNALS.flush_log_signal.connect(self.flush_log)
+
+        # Register UI signal handlers
+        self.__save_btn.clicked.connect(self.save_picture)
 
         # Create the signal dictionary
         self.command_signals = {
@@ -69,7 +82,17 @@ class MainWindow(QtWidgets.QMainWindow):
     @pyqtSlot(Image)
     def set_frame(self,image: Image):
         """ Sets the content of the image frame """
-        self.__frame.setPixmap(image.toqpixmap())
+        self.__picture = image
+        self.__frame.setPixmap(self.__picture.toqpixmap())
+
+    @pyqtSlot()
+    def save_picture(self):
+        if self.__picture is None:
+            return
+
+        fname,_ = QFileDialog.getSaveFileName(self,"Save picture as...","","PNG Images (*.png)")
+        if fname:
+            self.__picture.save(fname,"PNG")
 
 
 class MainThread(QThread):
@@ -82,6 +105,9 @@ class MainThread(QThread):
 
         # Spawn the window
         self.__window = MainWindow()
+
+        # Setup the stop event
+        self.__stop_event = Event()
 
         # Setup the protocol handler
         self.__handler = Protocol(SerialChannel("/dev/ttyACM0",19200,self.INTERVAL))
@@ -96,6 +122,12 @@ class MainThread(QThread):
     def run(self):
         """ Main loop of the client """
 
-        while True:
+        while not self.__stop_event.is_set():
             self.__handler.handle()
             time.sleep(self.INTERVAL)
+
+        self.__handler.stop()
+
+    def stop(self):
+        """ Stop the protocol handler """
+        self.__stop_event.set()
