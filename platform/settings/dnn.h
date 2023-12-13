7bf9706345985e87ecf3056b1564e6bee38448d1
Arshad Javeed <37022146+arshad171@users.noreply.github.com>
2023-12-13 13:18:34 +0100

    refactor

--- platform/settings/dnn.h
+++ platform/settings/dnn.h
@@ -1,4 +1,7 @@
-#define FEATURE_DIM 100
+// should match the dataset
+#define NUM_TRAINING_DATA 100
+// should match the expected/returned feature dims by the backend
+#define FEATURE_DIM 64
 #define MOMENTUM 0.2
 #define LEARNING_RATE 0.0001
 #define BATCH_SIZE 2
